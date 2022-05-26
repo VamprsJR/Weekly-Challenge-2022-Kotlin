64c4522fe827ecce9ba5becb8135a75c9e153c11
VamprsJR <tct.lol4xd2@gmail.com>
2022-05-26 16:33:36 -0400

    Update Challenge0.kt

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -35,3 +35,23 @@ fun main() {
         }
     }
 }
+
+
+""Solucion del reto 1 bajo el lenguaje de Python"
+
+def main():
+
+    for x in range(1,101):
+        if x % 3  == 0 and x  % 5 ==0:
+            print("fizzbuzz")
+        elif x % 5 == 0:
+            print("buzz")
+        elif x % 3 == 0:
+            print("fizz")
+
+        print(x)
+    
+
+main()
+
+
